a69bb593bc48b4e1d3a6dd0e3c7cd709b7ca0641
shubham shukla <shubham.shukla@3pillarglobal.com>
2022-08-31 21:25:59 +0530

    learning objects in js

---
+++ week2/object.js
@@ -0,0 +1,11 @@
+//Constructing empty objects
+
+const o1 = new Object();
+const o2 = new Object(undefined);
+const o3 = new Object(null);
+// equivalent to const o = new Boolean(true)
+const o4 = new Object(true);
+// equivalent to const o = new Boolean(false)
+const o5 = new Object(Boolean());
+
+
