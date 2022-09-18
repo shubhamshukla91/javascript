70dd9c40eb2240fbaeb723f21ed3fd369172e428
shubham shukla <shubham.shukla@3pillarglobal.com>
2022-09-18 11:43:27 +0530

    updates after using async await

--- task2/index.js
+++ task2/index.js
@@ -4,11 +4,13 @@ function storeDetails(){
     var lowerCaseLetters = /[a-z]/g;
     var upperCaseLetters = /[A-Z]/g;
     var numbers = /[0-9]/g;
-    if(name.length == 0 || password.length == 0 || !password.match(numbers)|| !password.match(upperCaseLetters) || !password.match(lowerCaseLetters)){
+    if(name.length == 0 || password.length == 0 || password.length<8){
+        // !password.match(numbers)|| !password.match(upperCaseLetters) || !password.match(lowerCaseLetters)){
         window.location.href="./index.html";
         window.location.reload(true)
-        window.location.replace("index.html")
+        window.location.replace("./index.html")
         alert("please fill in email & password correctly i.e., password must be of atleast 8 characters long including uppercase,lowercase letter and 1 number");
+        return
        
     }else{
         localStorage.setItem("name", name);
