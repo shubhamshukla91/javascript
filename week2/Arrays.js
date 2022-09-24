3b32cc8555cdd0bd31fe6029868da09b6f192a47
shubham shukla <shubham.shukla@3pillarglobal.com>
2022-09-24 08:49:11 +0530

    Added few more examples

--- week2/Arrays.js
+++ week2/Arrays.js
@@ -1,7 +1,7 @@
 //1 forEach
-const array = ['a', 'b', 'c'];
+const array = ["a", "b", "c"];
 
-array1.forEach(element => console.log(element));
+array1.forEach((element) => console.log(element));
 
 //2 fill
 
@@ -18,49 +18,45 @@ console.log(array1.fill(5, 1));
 console.log(array1.fill(6));
 // expected output: [6, 6, 6, 6]
 
-
 //3 group
 
 const inventory = [
-    { name: 'asparagus', type: 'vegetables', quantity: 5 },
-    { name: 'bananas',  type: 'fruit', quantity: 0 },
-    { name: 'goat', type: 'meat', quantity: 23 },
-    { name: 'cherries', type: 'fruit', quantity: 5 },
-    { name: 'fish', type: 'meat', quantity: 22 }
-  ];
+  { name: "asparagus", type: "vegetables", quantity: 5 },
+  { name: "bananas", type: "fruit", quantity: 0 },
+  { name: "goat", type: "meat", quantity: 23 },
+  { name: "cherries", type: "fruit", quantity: 5 },
+  { name: "fish", type: "meat", quantity: 22 },
+];
 
 const result = inventory.group(({ type }) => type);
 
 function myCallback({ quantity }) {
-    return quantity > 5 ? 'ok' : 'restock';
-  }
-  
-  result = inventory.group(myCallback);
+  return quantity > 5 ? "ok" : "restock";
+}
+
+result = inventory.group(myCallback);
 
 //4 indexOf
 
-const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+const beasts = ["ant", "bison", "camel", "duck", "bison"];
 
-console.log(beasts.indexOf('bison'));
+console.log(beasts.indexOf("bison"));
 // expected output: 1
 
 // start from index 2
-console.log(beasts.indexOf('bison', 2));
+console.log(beasts.indexOf("bison", 2));
 // expected output: 4
 
-console.log(beasts.indexOf('giraffe'));
+console.log(beasts.indexOf("giraffe"));
 // expected output: -1
-
-
 
 //5 push&pop
 
-const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
 
 console.log(plants.push(brinjal));
 
 console.log(plants.pop());
-
 
 //6 shift & sort
 
@@ -89,15 +85,20 @@ const even = arr.filter((v) => v % 2 === 0);
 const double = arr.map((v) => v * 2);
 // [10, 12, 26, 0, 2, 36, 46]
 
-
-
-const map1 = array3.map(x => x * 2);
+const map1 = array3.map((x) => x * 2);
 
 console.log(map1);
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
 
-const result1 = words.filter(word => word.length > 6);
+const result1 = words.filter((word) => word.length > 6);
 
 console.log(result1);
 
@@ -112,18 +113,112 @@ const sumWithInitial = array4.reduce(
 
 console.log(sumWithInitial);
 
+//map
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map((item) => item * 2);
+console.log(doubled);
+//filter
+const evens = numbers.filter((item) => item % 2 === 0);
+console.log(evens);
+//reduce
+const total = numbers.reduce(function (result, item) {
+  return result + item;
+}, 0);
+console.log(total);
+
+//map
+var officers = [
+  { id: 20, name: "Captain Piett" },
+  { id: 24, name: "General Veers" },
+  { id: 56, name: "Admiral Ozzel" },
+  { id: 88, name: "Commander Jerjerrod" },
+];
+var officersIds = [];
+officers.forEach((oofficer) => officersIds.push(oofficer.id));
+const officersIds = officers.map((officer) => officer.id);
+
+//reduce
+var pilots = [
+  {
+    id: 10,
+    name: "Poe Dameron",
+    years: 14,
+  },
+  {
+    id: 2,
+    name: "Temmin 'Snap' Wexley",
+    years: 30,
+  },
+  {
+    id: 41,
+    name: "Tallissan Lintra",
+    years: 16,
+  },
+  {
+    id: 99,
+    name: "Ello Asty",
+    years: 22,
+  },
+];
+const totalYears = pilots.reduce((acc, pilot) => acc + pilot.years, 0);
+var mostExpPilot = pilots.reduce(
+  (oldest, pilot) => ((oldest.years || 0) > pilot.years ? oldest : pilot),
+  {}
+);
+
+//combined
+var personnel = [
+  {
+    id: 5,
+    name: "Luke Skywalker",
+    pilotingScore: 98,
+    shootingScore: 56,
+    isForceUser: true,
+  },
+  {
+    id: 82,
+    name: "Sabine Wren",
+    pilotingScore: 73,
+    shootingScore: 99,
+    isForceUser: false,
+  },
+  {
+    id: 22,
+    name: "Zeb Orellios",
+    pilotingScore: 20,
+    shootingScore: 59,
+    isForceUser: false,
+  },
+  {
+    id: 15,
+    name: "Ezra Bridger",
+    pilotingScore: 43,
+    shootingScore: 67,
+    isForceUser: true,
+  },
+  {
+    id: 11,
+    name: "Caleb Dume",
+    pilotingScore: 71,
+    shootingScore: 85,
+    isForceUser: true,
+  },
+];
+const totalJediScore = personnel
+  .filter((person) => person.isForceUser)
+  .map((jedi) => jedi.pilotingScore + jedi.shootingScore)
+  .reduce((acc, score) => acc + score, 0);
 
 //8 find
 const array5 = [5, 12, 8, 130, 44];
 
-const found = array5.find(element => element > 10);
+const found = array5.find((element) => element > 10);
 
 console.log(found);
 // expected output: 12
 
-
 //9 slice and splice
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+const animals = ["ant", "bison", "camel", "duck", "elephant"];
 
 console.log(animals.slice(2));
 // expected output: Array ["camel", "duck", "elephant"]
@@ -144,17 +239,16 @@ console.log(animals.slice());
 // expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
 
 //10 join
-const elements = ['Fire', 'Air', 'Water'];
+const elements = ["Fire", "Air", "Water"];
 
 console.log(elements.join());
 // expected output: "Fire,Air,Water"
 
-console.log(elements.join(''));
+console.log(elements.join(""));
 // expected output: "FireAirWater"
 
-console.log(elements.join('-'));
+console.log(elements.join("-"));
 // expected output: "Fire-Air-Water"
-
 
 //11 include
 const array6 = [1, 2, 3];
--- week2/object.js
+++ week2/object.js
@@ -10,142 +10,152 @@ const o5 = new Object(Boolean());
 
 //freeze
 const obj = {
-    prop: 42
-  };
-  
-  Object.freeze(obj);
-  
-  obj.prop = 33;
-  // Throws an error in strict mode
-  
-  console.log(obj.prop);
-  // expected output: 42
+  prop: 42,
+};
 
-  //call,bind,apply
+Object.freeze(obj);
 
-  function Car(type, fuelType){
-	this.type = type;
-	this.fuelType = fuelType;
+obj.prop = 33;
+// Throws an error in strict mode
+
+console.log(obj.prop);
+// expected output: 42
+
+//call,bind,apply
+
+function Car(type, fuelType) {
+  this.type = type;
+  this.fuelType = fuelType;
 }
 
-function setBrand(brand){
-	Car.call(this, "convertible", "petrol");
-	this.brand = brand;
-	console.log(`Car details = `, this);
+function setBrand(brand) {
+  Car.call(this, "convertible", "petrol");
+  this.brand = brand;
+  console.log(`Car details = `, this);
 }
 
-function definePrice(price){
-	Car.call(this, "convertible", "diesel");
-	this.price = price;
-	console.log(`Car details = `, this);
+function definePrice(price) {
+  Car.call(this, "convertible", "diesel");
+  this.price = price;
+  console.log(`Car details = `, this);
 }
 
-const newBrand = new setBrand('Brand1');
+const newBrand = new setBrand("Brand1");
 const newCarPrice = new definePrice(100000);
 
 //without argument
 const newEntity = (obj) => console.log(obj);
 
-function mountEntity(){
-	this.entity = newEntity;
-	console.log(`Entity ${this.entity} is mounted on ${this}`);
+function mountEntity() {
+  this.entity = newEntity;
+  console.log(`Entity ${this.entity} is mounted on ${this}`);
 }
 
 mountEntity.call();
 
 //apply
-function Car(type, fuelType){
-	this.type = type;
-	this.fuelType = fuelType;
+function Car(type, fuelType) {
+  this.type = type;
+  this.fuelType = fuelType;
 }
 
-function setBrand(brand){
-	Car.apply(this, ["convertible", "petrol"]); //Syntax with array literal
-	this.brand = brand;
-	console.log(`Car details = `, this);
+function setBrand(brand) {
+  Car.apply(this, ["convertible", "petrol"]); //Syntax with array literal
+  this.brand = brand;
+  console.log(`Car details = `, this);
 }
 
-function definePrice(price){
-	Car.apply(this, new Array("convertible", "diesel")); //Syntax with array object construction
-	this.price = price;
-	console.log(`Car details = `, this);
+function definePrice(price) {
+  Car.apply(this, new Array("convertible", "diesel")); //Syntax with array object construction
+  this.price = price;
+  console.log(`Car details = `, this);
 }
 
-const newBrand1 = new setBrand('Brand1');
+const newBrand1 = new setBrand("Brand1");
 const newCarPrice1 = new definePrice(100000);
-
 
 //bind
 const module = {
-    x: 42,
-    getX: function() {
-      return this.x;
-    }
-  };
-  
-  const unboundGetX = module.getX;
-  console.log(unboundGetX()); // The function gets invoked at the global scope
-  // expected output: undefined
-  
-  const boundGetX = unboundGetX.bind(module);
-  console.log(boundGetX());
-  // expected output: 42
+  x: 42,
+  getX: function () {
+    return this.x;
+  },
+};
 
-  
-  //create
-  const person = {
-    isHuman: false,
-    printIntroduction: function() {
-      console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
-    }
-  };
-  
-  const me = Object.create(person);
-  
-  me.name = 'Matthew'; // "name" is a property set on "me", but not on "person"
-  me.isHuman = true; // inherited properties can be overwritten
-  
-  me.printIntroduction();
-  // expected output: "My name is Matthew. Am I human? true"
+const unboundGetX = module.getX;
+console.log(unboundGetX()); // The function gets invoked at the global scope
+// expected output: undefined
 
-  
-  //hasOwn
-  const object1 = {
-    prop: 'exists'
-  };
-  
-  console.log(Object.hasOwn(object1, 'prop'));
-  // expected output: true
-  
-  console.log(Object.hasOwn(object1, 'toString'));
-  // expected output: false
-  
-  console.log(Object.hasOwn(object1, 'undeclaredPropertyValue'));
-  // expected output: false
+const boundGetX = unboundGetX.bind(module);
+console.log(boundGetX());
+// expected output: 42
 
-  
-  //keys
-  const object3 = {
-    a: 'somestring',
-    b: 42,
-    c: false
-  };
-  
-  console.log(Object.keys(object3));
-  // expected output: Array ["a", "b", "c"]
+//create
+const person = {
+  isHuman: false,
+  printIntroduction: function () {
+    console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+  },
+};
 
-  
-  //seal
-  const object2 = {
-    property1: 42
-  };
-  
-  Object.seal(object2);
-  object2.property1 = 33;
-  console.log(object2.property1);
-  // expected output: 33
-  
-  delete object2.property1; // cannot delete when sealed
-  console.log(object2.property1);
-  // expected output: 33
-  
+const me = Object.create(person);
+
+me.name = "Matthew"; // "name" is a property set on "me", but not on "person"
+me.isHuman = true; // inherited properties can be overwritten
+
+me.printIntroduction();
+// expected output: "My name is Matthew. Am I human? true"
+
+//hasOwn
+const object1 = {
+  prop: "exists",
+};
+
+console.log(Object.hasOwn(object1, "prop"));
+// expected output: true
+
+console.log(Object.hasOwn(object1, "toString"));
+// expected output: false
+
+console.log(Object.hasOwn(object1, "undeclaredPropertyValue"));
+// expected output: false
+
+//keys
+const object3 = {
+  a: "somestring",
+  b: 42,
+  c: false,
+};
+
+console.log(Object.keys(object3));
+// expected output: Array ["a", "b", "c"]
+
+//seal
+const object2 = {
+  property1: 42,
+};
+
+Object.seal(object2);
+object2.property1 = 33;
+console.log(object2.property1);
+// expected output: 33
+
+delete object2.property1; // cannot delete when sealed
+console.log(object2.property1);
+// expected output: 33
+
+const person1 = {
+  firstname: "shubham",
+  lastname: "shukla ",
+  getName: function () {
+    let fullname = this.firstname + " " + this.lastname;
+    return fullname;
+  },
+};
+
+const displayPerson = function (snack, hobby) {
+  console.log(this.getName() + "loves" + snack + "and" + hobby);
+};
+
+displayPerson.call(person1, "burger", "algo");
+displayPerson.apply(person1, ["burger", "algo"]);
